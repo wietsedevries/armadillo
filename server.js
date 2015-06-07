2c802fb96a874a9038227e0b34c89a608006fbc7
Wietse <wietse.l.devries@gmail.com>
2015-06-07 18:07:29 +0200

    refactoring

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var armadillo = require('dillo');
-// app.use(express.logger('dev'));
+var dillo = require('dillo');
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.static(path.join(__dirname,'views')));
 var server = require('http').createServer(app).listen(3000);
 var io = require('socket.io').listen(server);
 io.set('log level',1);
 io.sockets.on('connection', function (socket) {
-    armadillo.initGame(io, socket);
+    dillo.initGame(io, socket);
 });
