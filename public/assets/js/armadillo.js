379f910202f6234fd1a69f166a5e6d5b9eb5407a
Wietse <wietse.l.devries@gmail.com>
2015-06-07 02:17:57 +0200

    Refactoring existing code and preparing code for deployment

--- public/assets/js/armadillo.js
+++ public/assets/js/armadillo.js
@@ -1,7 +1,7 @@
-// Detect device to prepare the correct landing page
-window.onload = function(){
+window.onload = function() {
   detectDevice();
 };
+
 function detectDevice() {
   if(  /Android|webOS|iPhone|iPod|BlackBerry|iPad/i.test(navigator.userAgent) ){
     $('#player').show();
@@ -9,217 +9,174 @@ function detectDevice() {
     $('#host').show();
   }
 }
+
 var App;
-var buttTime = 0;
+var offset;
 
-jQuery(function($){
-   'use strict';
+jQuery( function($) {
+  'use strict';
 
-   var IO = {
+  var IO = {
 
-        // Connect client to server
-        init: function() {
-            IO.socket = io.connect();
-            IO.bindEvents();
-        },
+    init: function() {
+        IO.socket = io.connect();
+        IO.bindEvents();
+    },
 
-        // Listen to events coming from the server
-        bindEvents : function() {
-            IO.socket.on('connected', IO.Connected );
-            IO.socket.on('gameCreated', IO.gameCreated );
-            IO.socket.on('joined', IO.joined );
-            IO.socket.on('clicked', IO.clicked );
-        },
+    bindEvents : function() {
+        IO.socket.on('connected', IO.Connected );
+        IO.socket.on('gameCreated', IO.gameCreated );
+        IO.socket.on('joined', IO.joined );
+        IO.socket.on('clicked', IO.clicked );
+    },
 
-        // Clientside is connected
-        Connected : function() {
-            // set session id
-            App.mySocketId = IO.socket.socket.sessionid;
-        },
+    Connected : function() {
+        App.socketId = IO.socket.socket.sessionid;
+    },
 
-        // A new game has been created and a random game ID has been generated.
-        gameCreated : function(data) {
-            App.Host.startGame(data);
-        },
+    gameCreated : function(data) {
+        App.Host.startGame(data);
+    },
 
-        // A player has successfully joined the game.
-        joined : function(data) {
-            // Update screens for both player and host
-            App[App.role].changeLayout(data);
-        },
+    joined : function(data) {
+        App[App.role].changeLayout(data);
+    },
 
-        // Click on button
-        clicked: function (data) {
-          App.Host.clickButton(data);
+    clicked: function(data) {
+      App.Host.clickAction(data);
+    }
+  };
+
+  App = {
+
+    activeRoom: 0,
+
+    role: '',
+
+    socketId: '',
+
+    init: function() {
+      App.bindEvents();
+      FastClick.attach(document.body);
+    },
+
+    endGame: function (score) {
+      $('#game').hide();
+      $('#getCode').hide();
+      $('#menu').hide();
+      $('#host').show();
+      $('#score').show();
+      $('#newScore').html(score + ' Points');
+
+    },
+
+    restart: function() {
+      restartGame();
+      $('#host').hide();
+      $('#game').show();
+
+    },
+
+		controller: function(x,y) {
+			App.$doc.on('touchstart', x, function() { App.Player.button(y); });
+			App.$doc.on('touchend', x, function() { App.Player.button(y); });
+		},
+
+    bindEvents: function() {
+      App.$doc = $(document);
+      App.$doc.on('click', '#play', App.Host.createGame);
+      App.$doc.on('click', '#playAgain', App.restart);
+      App.$doc.on('click', '#join',App.Player.onJoin);
+
+			// Controller functions
+      // To add more buttons to the controller just use this function:
+      // App.controller(UNIQUE_BUTTON_ID, UNIQUE_NUMBER);
+      // UNIQUE_BUTTON_ID being the id of the button
+      // and UNIQUE_NUMBER as an identifier
+
+      App.controller('#button1',1);
+			App.controller('#button2',2);
+      // Example: App.controller('#anyDiv',272);
+
+    },
+
+    Host : {
+
+      createGame: function() {
+        IO.socket.emit('createNewGame');
+      },
+
+      startGame: function(data) {
+        App.activeRoom = data.activeRoom;
+        App.socketId = data.socketId;
+        App.role = 'Host';
+        App.Host.showCode();
+      },
+
+      showCode : function() {
+        $('#menu').hide();
+				$('#getCode').show();
+        $('#code').html(App.activeRoom);
+      },
+
+      // Once the button has been received by the Host it will run the function
+      // with the asigned identifier on the controller.js file
+      clickAction: function(data) {
+        var cases = {
+            0: joystick,
+            1: button1,
+            2: button2
+            // Example: '272: functionName'
+        };
+        if (cases[data.button]) {
+            cases[data.button](data.angle);
         }
-    };
+			},
 
-    App = {
+      changeLayout: function(data) {
+        $('#host').hide();
+        $('#game').show();
+				start();
+      }
 
-        // gameId number
-        gameId: 0,
+    },
 
-        // This is used to differentiate between 'Host' and 'Player' browsers.
-        role: '',
+    Player : {
 
-        // Socket ID
-        mySocketId: '',
+		  setNum: 0,
 
-        // This runs when the page initially loads.
-        init: function () {
-            App.bindEvents();
+      onJoin: function() {
+        var data = {
+            activeRoom : $('#input').val(),
+        };
+        IO.socket.emit('JoinGame', data);
+        App.role = 'Player';
+      },
 
-            // Initialize the fastclick library
-            FastClick.attach(document.body);
-        },
-        // End game
-        endGame: function (score) {
-          $('#game').hide();
-          $('#getCode').hide();
-          $('#menu').hide();
-          $('#host').show();
-          $('#score').show();
-          $('#newScore').html(score+" Points");
+      changeLayout : function(data) {
+        if(IO.socket.socket.sessionid === data.socketId){
+          App.role = 'Player';
+          App.activeRoom = data.activeRoom;
 
-        },
-        // Restart game
-        restart: function () {
-          restartGame();
-          $('#host').hide();
-          $('#game').show();
-
-        },
-				// Button functions
-				controller: function (x,y) {
-					App.$doc.on('touchstart', x, function(){App.Player.button(y); });
-					App.$doc.on('touchend', x, function(){ App.Player.button(y); });
-				},
-
-        // Create some event handlers
-        bindEvents: function () {
-            App.$doc = $(document);
-            // Desktop
-            App.$doc.on('click', '#play', App.Host.createGame);
-            App.$doc.on('click', '#playAgain', App.restart);
-
-            // Player
-            App.$doc.on('click', '#join',App.Player.onJoin);
-
-						// Phonetroller (3 buttons, add more by adding more 'controller' functions)
-						App.controller('#button1',1);
-						App.controller('#button2',2);
-            // App.controller('#button3',3);
-
-        },
-
-        Host : {
-
-            //Handler for the "Start" button on the introScreen.
-            createGame: function () {
-                IO.socket.emit('createNewGame');
-            },
-            //The Host screen is displayed for the first time.
-            startGame: function (data) {
-                App.gameId = data.gameId;
-                App.mySocketId = data.mySocketId;
-                App.role = 'Host';
-								//show code
-                App.Host.showCode();
-            },
-
-            //Show the Host screen containing the game URL and code
-            showCode : function() {
-                //show desktop lobby
-                $('#menu').hide();
-								$('#getCode').show();
-
-                // Show the gameId on screen
-                $('#code').html(App.gameId);
-            },
-
-						//move player div on hostscreen
-						clickButton: function (data) {
-
-							switch (data.button) {
-                  case 0:
-                      joystick(data.angle);
-                      break;
-							    case 1:
-											button1();
-							        break;
-							    case 2:
-											button2();
-							        break;
-
-							}
-						},
-
-            //Update the Host screen when the first player joins
-            changeLayout: function(data) {
-
-                // show canvas and start game
-                $('#host').hide();
-                $('#game').show();
-								start();
-
-            }
-
-        },
-
-        Player : {
-
-						setNum: 0,
-
-            //The player entered their name and gameId (hopefully) and clicked Start.
-            onJoin: function() {
-
-                // collect data to send to the server
-                var data = {
-                    gameId : $('#input').val(),
-                };
-                // Send the gameId to the server
-                IO.socket.emit('JoinGame', data);
-
-                // Set data for the current player.
-                App.role = 'Player';
-            },
-
-            //Change show controller for player
-            changeLayout : function(data) {
-                if(IO.socket.socket.sessionid === data.mySocketId){
-                    App.role = 'Player';
-                    App.gameId = data.gameId;
-
-                    $('#connect').hide();
-                    $('#controller').show();
-										//set offset for joystick (needed to calculate angle of joystick)
-										offset = $( "#joystick" ).offset();
-
-                }
-            },
-
-						//Run function when controller buttons are clicked
-						button: function(x,y) {
-
-							//redefine data to include which button was pressed
-							var data = {
-                  gameId : App.gameId,
-									button : x,
-									angle : y
-              };
-							//Send data to server
-							IO.socket.emit('pressButton', data);
-
-              var n = new Date();
-              buttTime = n.getTime();
-            },
-
+          $('#connect').hide();
+          $('#controller').show();
+					offset = $('#joystick').offset();
 
         }
+      },
 
-    };
+			button: function(x,y) {
+				var data = {
+          gameId : App.gameId,
+					button : x,
+					angle : y
+        };
+				IO.socket.emit('pressButton', data);
+      },
+    }
+  };
 
-    IO.init();
-    App.init();
+  IO.init();
+  App.init();
 
 }($));
--- public/assets/js/controller.js
+++ public/assets/js/controller.js
@@ -1,65 +1,37 @@
-var angle,
-    jump,
-    left,
-    right,
-    tiny;
+var jump;
+var left;
+var right;
+var tiny;
 
-// Joystick  for angle//
-// function joystick(angle) {
-//
-//       if(angle !== 0 && (angle > -90 && angle < 90)) {
-//         right = true;
-//         left =  false;
-//       }else if (angle === 0){
-//         right = false;
-//         left =  false;
-//       }else{
-//         right = false;
-//         left =  true;
-//       }
-// }
-
-// joystick for hour
-function joystick(hour) {
-      switch (hour) {
-        case 0:
-          right = false;
-          left =  false;
-          break;
-        case 1: case 2: case 3: case 4: case 5: case 6:
-          right = true;
-          left =  false;
-          break;
-        case 7: case 8: case 9: case 10: case 11: case 12:
-          right = false;
-          left =  true;
-          break;
-        default:
-          right = false;
-          left =  false;
-          break;
-
-      }
-
-}
-
-
-// 'B' Button //
 function button1() {
-
-   tiny = true;
-
-
+  tiny = true;
 }
-// 'A' Button //
+
 function button2() {
   if (jump === true) {
     jump = false;
-    var n = new Date();
-    buttTime = (n.getTime() - buttTime);
-    console.log("button 1 & 2: " + buttTime+" ms");
-    buttTime = 0;
   }else{
     jump = true;
+  }
+}
+
+function joystick(hour) {
+  switch (hour) {
+    case 0:
+      right = false;
+      left =  false;
+      break;
+    case 1: case 2: case 3: case 4: case 5: case 6:
+      right = true;
+      left =  false;
+      break;
+    case 7: case 8: case 9: case 10: case 11: case 12:
+      right = false;
+      left =  true;
+      break;
+    default:
+      right = false;
+      left =  false;
+      break;
   }
 }
