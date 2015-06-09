8545b91c4668338b5dd0480355a2a8b6e575f5a2
Wietse <wietse.l.devries@gmail.com>
2015-06-09 23:55:49 +0200

    changed some things

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var dillo = require('dillo');
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.static(path.join(__dirname,'views')));
-var server = require('http').createServer(app).listen(80);
+var server = require('http').createServer(app).listen(3000);
 var io = require('socket.io').listen(server);
 io.set('log level',1);
 io.sockets.on('connection', function (socket) {
